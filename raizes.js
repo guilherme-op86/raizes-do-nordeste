a76e480caec0e6c30c1d2b1faaf062d0447c03ad
Guilherme Oliveira <guilherme-op@hotmail.com>
2026-06-07 23:32:59 -0300

    criado o carrinho, a contagem dos itens no carrinho, a relacao de itens adicionados ao carrino e a soma do valor total

--- raizes.js
+++ raizes.js
@@ -1,9 +1,10 @@
 const botoes = document.querySelectorAll(".btn")
+const contador = document.querySelector(".contador")
 
 botoes.forEach(botao =>{
     botao.addEventListener("click", () =>{
         const nome = botao.dataset.nome
-        const preco = botao.dataset.preco
+        const preco = Number(botao.dataset.preco)
 
         adicionarCarrinho(nome, preco)
     })
@@ -15,4 +16,40 @@ const adicionarCarrinho = (nome, preco) =>{
     carrinho.push({nome, preco})
 
     localStorage.setItem("carrinho", JSON.stringify(carrinho))
+
+    contadorCarrinho()
+}
+
+const contadorCarrinho = () =>{
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+
+    const carrinhoContador = carrinho.length
+
+    contador.innerText = carrinhoContador
+}
+
+window.addEventListener("load", () =>{
+    contadorCarrinho()
+})
+
+const loadLGPD = () =>{ 
+    alert("Todos os dados são armazenados conforme Lei nº 13.709/2018 (LGPD - Lei Geral de Proteção de dados")
+
+}
+
+const carregarCarrinho = () =>{
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+    const lista = document.querySelector("#listaCarrinho")
+    const totalSpan = document.querySelector("#total")
+
+    let total = 0
+    lista.innerHTML = ""
+
+    carrinho.forEach(item =>{
+        lista.innerHTML += `<p>${item.nome} - R$ ${item.preco} </p>`
+        total += Number(item.preco)
+    })
+
+    totalSpan.innerText = `Total: R$ ${total}`
+    
 }
