f938fa1257b969054de04f84367e7c6dc2cfa0f9
Guilherme Oliveira <guilherme-op@hotmail.com>
2026-06-16 11:35:05 -0300

    adicionando ícones de + e - ao carrinho

--- carrinho.js
+++ carrinho.js
@@ -9,29 +9,45 @@ const carregarCarrinho = () =>{
 
     carrinho.forEach(item =>{
 
+        //cria a imagem do prato
         const novaImagem = document.createElement("img")
+        //cria os parágrafos com nome do prato e preço
         const novoParagrafo = document.createElement("p")
         const novoParagrafo2 = document.createElement("p")
+        //cria a div que irá conter a imagem do prato e a div dadosPrato
         const novaDiv = document.createElement("div")
-        novaDiv.classList = "infoPrato"
+        //cria a div que irá conter o nome do prato e preço para aplicar display flex
         const novaDiv2 = document.createElement("div")
+        novaDiv.classList = "infoPrato"
+        //const novaDiv2 = document.createElement("div")
         novaDiv2.classList = "dadosPrato"
+
+        //bloco que vai criar o contador de pratos com botões + e -
+        const divContador = document.createElement("div")
+        const iconMais = document.createElement("i")
+        const iconMenos = document.createElement("i")
+        divContador.classList.add("divContador")
+        iconMenos.classList.add("fa-solid", "fa-minus")
+        iconMais.classList.add("fa-solid", "fa-plus")
+        divContador.append(iconMenos, iconMais)
 
 
         const novoTexto = document.createTextNode(`${dicionario[item.nome]}`)
         const preco = document.createTextNode(`R$ ${item.preco}`)
         novoParagrafo.appendChild(novoTexto)
         novoParagrafo2.appendChild(preco)
-        novaDiv2.appendChild(novoParagrafo)
-        novaDiv2.appendChild(novoParagrafo2)
+        novaDiv2.append(novoParagrafo, novoParagrafo2)
+        //novaDiv2.appendChild(novoParagrafo)
+        //novaDiv2.appendChild(novoParagrafo2)
 
 
         novaImagem.src = `imagens/${item.nome}.png`
         novaImagem.alt = `${dicionario[item.nome]}`
         novaImagem.classList = "imagemPrato"
         
-        novaDiv.appendChild(novaImagem)
-        novaDiv.appendChild(novaDiv2)
+        novaDiv.append(novaImagem, novaDiv2, divContador)
+        //novaDiv.appendChild(novaImagem)
+        //novaDiv.appendChild(novaDiv2)
 
         lista.appendChild(novaDiv)
 
