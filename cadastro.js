317c6be381821b8716c90bda73875cb845f4cf98
Guilherme Oliveira <guilherme-op@hotmail.com>
2026-06-23 23:31:37 -0300

    mensagens de cadastro rrealizado e confirmação de pagamento estao ok; falta apenas a opcao de escolha da filial.

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,21 @@
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click", () =>{
+    mostrarMensagem("Cadastro realizado com sucesso.")
+})
+
+
+const mostrarMensagem = (texto) =>{
+    const mensagem = document.querySelector("#mensagem")
+    mensagem.textContent = texto
+    mensagem.classList.add("show")
+
+    setTimeout(() =>{
+        mensagem.classList.remove("show")
+    }, 3000)
+
+}
+
 const loadLGPD = () =>{ 
     alert("Todos os dados são armazenados conforme Lei nº 13.709/2018 (LGPD - Lei Geral de Proteção de dados")
 }
---
+++ pagamento.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click", () =>{
+    alert("Ao confirmar, você aceita compartilhar suas informações para a conclusão do pagamento.")
+})
